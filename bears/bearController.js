7e35996b4e0bc3c1de3747feb7d306915740a299
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-06-05 12:48:52 -0400

    Implemented put and delete functionality in controller file.

--- bears/bearController.js
+++ bears/bearController.js
@@ -28,7 +28,7 @@ router
   .route('/:id')
   .get((req, res) => {
     const { id } = req.params;
-    Bear.findById(id)
+    Bear.findById(id) // find a specific resource in a collection by ID
       .then(foundBear => {
         res.status(200).json(foundBear);
       })
@@ -37,12 +37,33 @@ router
       })
   })
   .delete((req, res) => {
-    res.status(200).json({ status: 'please implement DELETE functionality' });
-    // findByIdAndRemove
+    const { id } = req.params;
+    Bear.findByIdAndRemove(id)
+      .then(removeBear => {
+        if (removeBear === null) {
+          res.status(404).json({ error: 'No Bear by that Id found in DB.' });
+          return;
+        }
+        res.json({ success: 'Bear removed successfully.', resource: removeBear });
+      })
+      .catch(err => {
+        res.status(500).json({ error: err });
+      })
   })
+
   .put((req, res) => {
     res.status(200).json({ status: 'please implement PUT functionality' });
-    // findByIdAndUpdate
+    const { id } = req.params;
+    const updates = ({ species, latinName } = req.body);
+    console.log('Updates', updates);
+    Bear.findByIdAndUpdate(id, updates, { new: true })
+      .then(updatedBear => {
+        if (updatedBear === null) {
+          res.status(404).json({ error: 'No Bear by that ID found in DB.' });
+        }
+        res.json({ success: 'Bear updated.', resource: updatedBear })
+      })
+      .catch(err => res.status(500).json({ error: err }))
   });
 
 module.exports = router;
